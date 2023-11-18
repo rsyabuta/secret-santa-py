2fd36396dca651c2921b949f8bb130c67588163e
Ryan Yabuta <rs.yabuta@gmail.com>
2023-11-18 01:25:08 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import csv
+import random
+import smtplib
+from email.mime.text import MIMEText
+
+class Entry:
+    name = ""
+    email = ""
+    address = ""
+    pick = ""
+
+    def __init__(self, name, email, address):
+        self.name = name
+        self.email = email
+        self.address = address
+
+    def select_pick(self, pool):
+        cleaned_pool = [x for x in pool if x.name != self.name]
+        # Make sure the last person doesn't get themselves
+        if len(cleaned_pool) == 2:
+            i = 1
+        else:
+            i = random.randrange(len(cleaned_pool))
+        self.pick = cleaned_pool[i]
+        return [x for x in pool if x != cleaned_pool[i]]
+
+def generate_entries(file):
+    with open(file) as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        entries = []
+        for row in csv_reader:
+            entries.append(Entry(row["Name"], row["Email"], row["Address"]))
+    return entries
+
+def generate_pool(entries):
+    pool = []
+    for entry in entries:
+        pool.append(entry.name)
+    return pool
+
+def generate_email(entry):
+    return """
+    Hello {},
+    
+    You have been selected to be {}'s secret santa! Please send your gift to {}.
+    
+    Happy Holidays!""".format(entry.name, entry.pick.name, entry.pick.address)
+
+def main():
+    entries = generate_entries("2023.csv")
+    pool = entries
+    for entry in entries:
+        pool = entry.select_pick(pool)
+        print(entry.name + " has " + entry.pick.name)
+
+if __name__ == '__main__':
+    main()
